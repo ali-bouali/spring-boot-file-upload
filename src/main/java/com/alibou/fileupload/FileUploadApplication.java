d28b331c13fcf1a60a8a9a302a9e113a5579ea51
ali-bouali <bouali.pro@gmail.com>
2023-10-02 13:00:36 +0200

    init project

---
+++ src/main/java/com/alibou/fileupload/FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibou/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
